0db5dcbfa56e05fed103abf822c34cc65f3ec539
Nguyễn Dương Kỳ Anh <86228617+KyAnh2001@users.noreply.github.com>
2023-06-08 20:11:30 +0700

    Final update

--- Car_Management/Admin/car_management/views.py
+++ Car_Management/Admin/car_management/views.py
@@ -4,11 +4,14 @@ from allauth.account.views import PasswordSetView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import PasswordChangeView
 from django.db.models import Sum
+from django.db.models.functions import TruncMonth
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import View
 
 from car.models import Products, Order, Customer
+
+from car.models import StatisticsProducts
 
 
 # utillity
@@ -43,10 +46,58 @@ class SaasView(LoginRequiredMixin, View):
         earnings_last_month = calculate_earnings_for_month(this_month - 1)
         # get percentage change
         percentage_change = ((earnings_this_month - earnings_last_month) / earnings_last_month) * 100
-
+        percentage_change = float("{:.2f}".format(percentage_change))
 
         for order_ in order:
             total += order_.total_price
+
+        earnings_per_month = Order.objects.filter(date_created__year=2023). \
+            annotate(month=TruncMonth('date_created')). \
+            values('month').annotate(total_earnings=Sum('total_price')) \
+            .order_by('month')
+        # print(earnings_per_month)
+        for entry in earnings_per_month:
+            month = entry['month'].strftime('%B')
+            earnings = entry['total_earnings']
+            print(f"{month}: ${earnings}")
+
+        earn_per_month = [entry['total_earnings'] for entry in earnings_per_month]
+        month = [entry['month'].strftime('%B') for entry in earnings_per_month]
+
+        car_accessories = Products.objects.filter(category='car accessories').count()
+        car_accessories = int(car_accessories)
+
+        car_care = Products.objects.filter(category='car care services').count()
+        car_care = int(car_care)
+
+        car_exterior = Products.objects.filter(category='car exterior').count()
+        car_exterior = int(car_exterior)
+
+        car_interior = Products.objects.filter(category='car interior').count()
+        car_interior = int(car_interior)
+
+        car_parts = Products.objects.filter(category='car parts').count()
+        car_parts = int(car_parts)
+
+        products_list = ['car accessories', 'car care services', 'car exterior', 'car interior', 'car parts']
+        products_number = [car_accessories, car_care, car_exterior, car_interior, car_parts]
+
+        statistic = StatisticsProducts.objects.all().order_by('-total_revenue')[:1]
+
+        for product in statistic:
+            product_name = product.product.title
+            quantity_sold = product.quantity_sold
+            total_rev = product.total_revenue
+
+        total_revs = StatisticsProducts.objects.aggregate(Sum('quantity_sold'))
+        total_value = total_revs['quantity_sold__sum']
+        # print(quantity_sold)
+        # print(product_name)
+        # print(total_revenue)
+        print(total_value)
+
+        selling_label = [product_name, 'Total']
+        selling_quantity = [quantity_sold, total_value]
 
         context = {
             'heading': "Car Management",
@@ -55,6 +106,17 @@ class SaasView(LoginRequiredMixin, View):
             'order': order,
             'customer': customer,
             'total': total,
+            'products_list': products_list,
+            'products_number': products_number,
+            'earn_per_month': earn_per_month,
+            'month': month,
+            'earnings_this_month': earnings_this_month,
+            'earnings_last_month': earnings_last_month,
+            'percentage_change': percentage_change,
+            'total_rev': total_rev,
+            'product_name': product_name,
+            'selling_label': selling_label,
+            'selling_quantity': selling_quantity,
         }
         return render(request, 'dashboard/dashboard-saas.html', context)
 
